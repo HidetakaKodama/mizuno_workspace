c9620f8f329eb44a53aad0d22176ece4c6336906
h-mizuno0730 <h.mizuno@webedge.jp>
2022-04-06 15:15:42 +0900

    第5章、第6章 練習問題：第２部 を終えました。

--- Ex/src/Ex3/Ex3_A1.java
+++ Ex/src/Ex3/Ex3_A1.java
@@ -1,16 +1,26 @@
 package Ex3;
 
 public class Ex3_A1 {
+		public static void main(String[] args) {
 
 //	for文
-	public static void main(String[] args) {
-		for(int i = 1; i < 6; i++) {
-			System.out.println(i * 3);
-		}
-	}
+
+//		for(int i = 1; i < 6; i++) {
+//			System.out.println(i * 3);
+//		}
+//	}
+
+//	while文
+//	int i = 1;
+//	while ( i < 6) {
+//		System.out.println(i * 3);
+//		i++;
+//	}
 
 	int i = 1;
-	while ( i < 6) {
-		System.out.println(i);
-	}
+	do {
+		System.out.println(i * 3);
+		i++;
+	} while (i < 6);
+}
 }
