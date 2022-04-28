98c805c808bedcbf145b5e82b04bf346eb3f28f9
h-mizuno0730 <h.mizuno@webedge.jp>
2022-04-28 18:11:53 +0900

    4/28 Java応用（Aのみ）終了。

--- Ex_Java_ouyou/src/ex4/DBAccessEx4.java
+++ Ex_Java_ouyou/src/ex4/DBAccessEx4.java
@@ -7,23 +7,25 @@ import java.sql.SQLException;
 import java.util.Scanner;
 public class DBAccessEx4 {
 	public static void main(String[] main) throws SQLException {
-		System.out.println("会社IDを変更する研修生のIDを入力して下さい。");
-		Scanner sc = new Scanner(System.in);
-		int co_id1 = sc.nextInt();
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("会社IDを変更する研修生のIDを入力して下さい。");
+		int id = sc.nextInt();
+
+		System.out.println("変更後の会社IDを入力してください");
+		int co_id = sc.nextInt();
 		String url = "jdbc:mysql://localhost/testsql";
 		String user = "root";
 		String password = "";
-		Connection con = DriverManager.getConnection(url,user,password);
-		PreparedStatement pstmt = con.prepareStatement("select co_id from traine  where id = ?");{
-		pstmt.setInt(1, co_id1);
-
-		System.out.println("変更後の会社IDを入力してください");
-		int co_id2 = sc.nextInt();
-		PreparedStatement pstmt2 = con.prepareStatement("UPDATE trainee SET co_id = ?");{
-			pstmt2.setInt(1, co_id2);
-
-		}
-        int rows = pstmt2.executeUpdate();
+		try (
+			Connection con = DriverManager.getConnection(url,user,password);
+			PreparedStatement pstmt = con.prepareStatement("UPDATE trainee SET co_id = ? where id = ?")){
+				pstmt.setInt(1, id);
+		        pstmt.setInt(2, co_id);
+                int rows = pstmt.executeUpdate();
+			} catch (SQLException e){
+				e.printStackTrace();
 		}
 	}
 }
